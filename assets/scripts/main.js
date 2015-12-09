6b27f50adc563dbf8a3c7c410493d7a635903a52
Diego Vera <diego.vera@possible.com>
2015-12-08 20:58:03 -0600

    gato

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
 
     $('.start-game').on('click', function(e) {
+        $(this).attr('disabled', 'disabled');
+        $('.stop-game').removeAttr('disabled');
         console.log('start game');
         startGame();
     });
 
     $('.stop-game').on('click', function(e) {
+        $(this).attr('disabled', 'disabled');
+        $('.start-game').removeAttr('disabled');
         console.log('stop game');
-        //$(this).unbind('click');
+        stopGame();
     });
 
 });
@@ -24,26 +28,44 @@ function startGame() {
 
     $('.matrix').on('click', '.matrix-item',function(e) {
         console.log($(this).index());
-        $(this).addClass('inactive');
-        $(this).removeClass('active');
 
-        if(checkPlayer() == 'player1') {
+        if(checkPlayer() == 'player1' && $(this).hasClass('active')) {
+            $(this).addClass('cross');
+            hasWin( checkPlayer() );
+
             $('.current-player').text(players[1]);
             $('.current-player').removeClass('py1');
-            $('.current-player').addClass('py2');
+            $('.current-player').addClass('py2'); 
         }
 
-        else if(checkPlayer() == 'player2') {
+        else if(checkPlayer() == 'player2' && $(this).hasClass('active')) {
+            $(this).addClass('circle');
+            hasWin( checkPlayer() );
+
             $('.current-player').text(players[0]);
             $('.current-player').removeClass('py2');
             $('.current-player').addClass('py1');
+            $(this).addClass('circle');
         }
+
+        $(this).addClass('inactive');
+        $(this).removeClass('active');
+
     });
 };
 
 
 function stopGame() {
 
+    $('.matrix').unbind('click');
+
+    $('.matrix-item')
+        .removeClass('inactive cross circle win')
+        .addClass('active');
+
+    $('.current-player')
+        .removeClass('py1 py2')
+        .text('');
 };
 
 function checkPlayer() {
@@ -57,6 +79,111 @@ function checkPlayer() {
         return 'player2';
     }
 };
+
+function hasWin(player) {
+
+    var itemSign = '';
+
+    if(player == 'player1') {
+        itemSign = 'cross';
+    }
+
+    else if(player == 'player2') {
+        itemSign = 'circle';
+    }
+
+    var itemsArray = $('.matrix .matrix-item');
+
+    if( $(itemsArray).eq(0).hasClass(itemSign) &&
+        $(itemsArray).eq(1).hasClass(itemSign) &&
+        $(itemsArray).eq(2).hasClass(itemSign) ) {
+
+        $(itemsArray).eq(0).addClass('win');
+        $(itemsArray).eq(1).addClass('win');
+        $(itemsArray).eq(2).addClass('win');
+
+        $('.matrix').unbind('click');
+    }
+
+    if( $(itemsArray).eq(3).hasClass(itemSign) &&
+        $(itemsArray).eq(4).hasClass(itemSign) &&
+        $(itemsArray).eq(5).hasClass(itemSign) ) {
+
+        $(itemsArray).eq(3).addClass('win');
+        $(itemsArray).eq(4).addClass('win');
+        $(itemsArray).eq(5).addClass('win');
+
+        $('.matrix').unbind('click');
+    }
+
+    if( $(itemsArray).eq(6).hasClass(itemSign) &&
+        $(itemsArray).eq(7).hasClass(itemSign) &&
+        $(itemsArray).eq(8).hasClass(itemSign) ) {
+
+        $(itemsArray).eq(6).addClass('win');
+        $(itemsArray).eq(7).addClass('win');
+        $(itemsArray).eq(8).addClass('win');
+
+        $('.matrix').unbind('click');
+    }
+
+    if( $(itemsArray).eq(0).hasClass(itemSign) &&
+        $(itemsArray).eq(3).hasClass(itemSign) &&
+        $(itemsArray).eq(6).hasClass(itemSign) ) {
+
+        $(itemsArray).eq(0).addClass('win');
+        $(itemsArray).eq(3).addClass('win');
+        $(itemsArray).eq(6).addClass('win');
+
+        $('.matrix').unbind('click');
+    }
+
+    if( $(itemsArray).eq(2).hasClass(itemSign) &&
+        $(itemsArray).eq(5).hasClass(itemSign) &&
+        $(itemsArray).eq(8).hasClass(itemSign) ) {
+
+        $(itemsArray).eq(2).addClass('win');
+        $(itemsArray).eq(5).addClass('win');
+        $(itemsArray).eq(8).addClass('win');
+
+        $('.matrix').unbind('click');
+    }
+
+    if( $(itemsArray).eq(0).hasClass(itemSign) &&
+        $(itemsArray).eq(4).hasClass(itemSign) &&
+        $(itemsArray).eq(8).hasClass(itemSign) ) {
+
+        $(itemsArray).eq(0).addClass('win');
+        $(itemsArray).eq(4).addClass('win');
+        $(itemsArray).eq(8).addClass('win');
+
+        $('.matrix').unbind('click');
+    }
+
+    if( $(itemsArray).eq(2).hasClass(itemSign) &&
+        $(itemsArray).eq(4).hasClass(itemSign) &&
+        $(itemsArray).eq(6).hasClass(itemSign) ) {
+
+        $(itemsArray).eq(2).addClass('win');
+        $(itemsArray).eq(4).addClass('win');
+        $(itemsArray).eq(6).addClass('win');
+
+        $('.matrix').unbind('click');
+    }
+
+    if( $(itemsArray).eq(1).hasClass(itemSign) &&
+        $(itemsArray).eq(4).hasClass(itemSign) &&
+        $(itemsArray).eq(7).hasClass(itemSign) ) {
+
+        $(itemsArray).eq(1).addClass('win');
+        $(itemsArray).eq(4).addClass('win');
+        $(itemsArray).eq(7).addClass('win');
+
+        $('.matrix').unbind('click');
+    }
+};
+
+
 
 
 
